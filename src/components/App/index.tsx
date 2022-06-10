93e5e674c3461517016cdbb679811c4ac31415cf
iamthe-Wraith <allhailthewraith@protonmail.com>
2022-06-10 09:42:40 -0400

    adds coming soon component to replace static message

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,17 +1,14 @@
 import React from 'react';
 import { ThemeStore } from '../../contexts/theme-store';
 import { GlobalStyles } from '../../styles/styles';
+import { ComingSoon } from '../ComingSoon';
 import { ThemeProvider } from '../ThemeProvider';
-
-import { AppContainer } from './styles';
 
 export const App: React.FC = () => (
   <ThemeStore>
     <ThemeProvider>
       <GlobalStyles />
-      <AppContainer>
-        temp App container
-      </AppContainer>
+      <ComingSoon />
     </ThemeProvider>
   </ThemeStore>
 );
