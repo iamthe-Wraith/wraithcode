5b2064bf616212e416fe666ce33d22010247de2b
iamthe-Wraith <allhailthewraith@protonmail.com>
2024-03-12 13:12:02 -0400

    build: resolves env vars not being passed to container on build and run

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang=ts>
-	import { PUBLIC_TEST_SECRET } from "$env/static/public";
+	import { PUBLIC_APP_ENV } from "$env/static/public";
 </script>
 
 <h1>Welcome to Wraithcode</h1>
 <p>Things are in the works...come back soon to see what we are able to accomplish!</p>
-<p>super secret: { PUBLIC_TEST_SECRET }</p>
+<p>env: { PUBLIC_APP_ENV }</p>
